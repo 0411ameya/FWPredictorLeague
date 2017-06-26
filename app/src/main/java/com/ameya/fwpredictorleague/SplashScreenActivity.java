f90864b7ee1165a615aee240f85a3a9bbba68a61
Ameya Daphalapurkar <0411ameya@gmail.com>
2017-06-25 23:46:29 -0400

    Add splash screen and set up nav bar

---
+++ app/src/main/java/com/ameya/fwpredictorleague/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.ameya.fwpredictorleague;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                    startActivity(new Intent(SplashScreenActivity.this,HomeActivity.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+    }
+}
